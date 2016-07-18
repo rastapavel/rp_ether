646171ac6d75a8d069f4ec0da16e15dab4ae288b
rastapavel <rastapavel76@gmail.com>
2016-07-18 15:09:12 +0200

    Barebones version of multiauth_limits.se. Insecure, but works

--- src/test_limits.py
+++ src/test_limits.py
@@ -1,58 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jul 13 17:58:50 2016
-
-@author: https://github.com/mc2-umd/ethereumlab/blob/master/Examples/mutual_credit_system.py
+Created on Wed Jul 18 17:58:50 2016
+Author: RP
 """
 
 import serpent
 from ethereum import tester as t, utils as u, abi
 
-#%%
-
-# From original UofM tutorial
-public_k0 = u.privtoaddr(t.k0)
-public_k1 = u.privtoaddr(t.k1)
-print("whassup")
-
-
 s = t.state()
 c = s.abi_contract("multiauth_limits.se")
 
+# Create limit accounts and query contents
+c.init_limit(0, 100, 1000)
+c.ask_limit(0)
+c.ask_balance(0)
+c.ask_sender(0)
+c.init_limit(1, 500, 1000)
+c.ask_limit(1)
+c.ask_balance(0)
+c.ask_sender(1)
 
-#
-#o = c.balance(public_k0)
-#print("tester.k0's current balance is " + str(o))
-#
-#o = c.balance(public_k1)
-#print("tester.k1's current balance is " + str(o))
-#
-#o = c.transfer(public_k0, 500, sender=tester.k1)
-#if o == 1:
-#	print("500 credits sent to tester_k1 from tester_k0")
-#else:
-#	print("Failed to send 500 credits to tester_k1 from tester_k0")
-#
-#o = c.balance(public_k0)
-#print("tester.k0's current balance is " + str(o))
-#
-#o = c.balance(public_k1)
-#print("tester.k1's current balance is " + str(o))
-#
-#o = c.transfer(public_k0, 1500, sender=tester.k1)
-#if o == 1:
-#	print("1500 credits sent to tester_k1 from tester_k0")
-#else:
-#	print("Failed to send 1500 credits to tester_k1 from tester_k0")
-#
-#o = c.transfer(public_k0, 500, sender=tester.k1)
-#if o == 1:
-#    print("500 credits sent to tester_k1 from tester_k0")
-#else:
-#    print("Failed to send 500 credits to tester_k1 from tester_k0")
-#
-#o = c.balance(public_k0)
-#print("tester.k0's current balance is " + str(o))
-#
-#o = c.balance(public_k1)
-#print("tester.k1's current balance is " + str(o))
+# Transfer
+b00 = c.transfer(0, 100) # Valid
+b01 = c.transfer(0,901) # Invalid, since results in negative balance
+
+b1 = c.transfer(1, 501) #Invalid, since above user 1's limit of 500
